46237839b32a6915ef26aaf25bebce741a46da17
Nicolas "Pixel" Noble <pixel@nobis-crew.org.>
2025-04-04 04:57:11 +0000

    Documentation.

---
+++ search/concepts_0.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['psyqo_3a_3aconcepts_3a_3aiscdromdevicestateenum_0',['IsCDRomDeviceStateEnum',['../da/d91/conceptpsyqo_1_1Concepts_1_1IsCDRomDeviceStateEnum.html',1,'psyqo::Concepts']]],
+  ['psyqo_3a_3aenum_1',['Enum',['../d3/d56/conceptpsyqo_1_1Enum.html',1,'psyqo']]],
+  ['psyqo_3a_3afragment_2',['Fragment',['../d5/d5c/conceptpsyqo_1_1Fragment.html',1,'psyqo']]],
+  ['psyqo_3a_3aprimitive_3',['Primitive',['../dd/d30/conceptpsyqo_1_1Primitive.html',1,'psyqo']]]
+];
---
+++ search/defines_3.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['default_5ftty_5finstall_0',['DEFAULT_TTY_INSTALL',['../dc/d03/tty_8c.html#aa91ea97c9997ea20749ec8b88acc8d00',1,'tty.c']]],
+  ['dicr_1',['DICR',['../de/d57/hwregs_8h.html#ac1c5a03345a77fcf1c179691ca73ef93',1,'hwregs.h']]],
+  ['dma_5fctrl_2',['DMA_CTRL',['../d9/d95/dma_8h.html#a5d5aacf18793eba82b736c160d30f0db',1,'dma.h']]],
+  ['dpcr_3',['DPCR',['../de/d57/hwregs_8h.html#a16a6dbd07aa9f49d1b164250247fd453',1,'hwregs.h']]],
+  ['dprintf_4',['dprintf',['../dd/d44/psyqo_2src_2alloc_8c.html#a0fb7bf82c787085c8b1ef0ad7d028424',1,'alloc.c']]]
+];
