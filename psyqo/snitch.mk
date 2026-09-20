41978b549748f7ddfbcfb5262d26bedb0b4859a2
Rix <rix@nobis-crew.org>
2026-09-20 15:35:36 -0700

    Bring libcester, snitch and arith64 in so the tests build standalone.

--- psyqo/snitch.mk
+++ psyqo/snitch.mk
@@ -2,7 +2,7 @@ ifndef SNITCHDIR
 SNITCHDIR := $(dir $(abspath $(lastword $(MAKEFILE_LIST))))snitch/
 
 LIBRARIES += $(SNITCHDIR)libsnitch.a
-CPPFLAGS += -I$(SNITCHDIR)../../../../third_party/snitch
+CPPFLAGS += -I$(SNITCHDIR)../../third_party/snitch
 
 include $(SNITCHDIR)../../psyqo/psyqo.mk
 
